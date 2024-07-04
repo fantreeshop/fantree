e26582c7681f316071624a96563dff5bc98b4fd2
fantreeshop <onlinefantreeshop@gmail.com>
2024-07-04 21:44:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en-gb" dir="ltr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/png" href="static/image/favicon.png" >
+<link rel="preconnect" href="//www.googletagmanager.com" crossorigin>
+<link rel="preconnect" href="//pagead2.googlesyndication.com" crossorigin>
+<style>
+@charset "UTF-8";html{font-family:-apple-system,BlinkMacSystemFont,segoe ui,Roboto,helvetica neue,Arial,noto sans,sans-serif,apple color emoji,segoe ui emoji,segoe ui symbol,noto color emoji;font-size:16px;font-weight:400;line-height:1.5;-webkit-text-size-adjust:100%;background:#fff;color:#323247;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased}body{margin:0}a{color:#323247;text-decoration:none}iframe,img{vertical-align:middle}img{max-width:100%;height:auto;box-sizing:border-box}iframe{border:0}p,ul{margin:0 0 20px}*+p{margin-top:20px}h1,.uk-heading-medium{margin:0 0 20px;font-family:-apple-system,BlinkMacSystemFont,segoe ui,Roboto,helvetica neue,Arial,noto sans,sans-serif,apple color emoji,segoe ui emoji,segoe ui symbol,noto color emoji;font-weight:700;color:#333;text-transform:none}*+h1,*+.uk-heading-medium{margin-top:50px}h1{font-size:1.9125rem;line-height:1.2}@media (min-width:960px){h1{font-size:2.25rem}}ul{padding-left:30px}:root{--uk-breakpoint-s: 640px;--uk-breakpoint-m: 960px;--uk-breakpoint-l: 1200px;--uk-breakpoint-xl: 1600px}.uk-heading-medium{font-size:2.875rem;line-height:1.1}@media (min-width:960px){.uk-heading-medium{font-size:3.75rem}}@media (min-width:1200px){.uk-heading-medium{font-size:4rem}}.uk-section{box-sizing:border-box;padding-top:40px;padding-bottom:40px}@media (min-width:960px){.uk-section{padding-top:60px;padding-bottom:60px}}.uk-section::before,.uk-section::after{content:"";display:table}.uk-section::after{clear:both}.uk-section>:last-child{margin-bottom:0}.uk-container{box-sizing:content-box;max-width:1200px;margin-left:auto;margin-right:auto;padding-left:15px;padding-right:15px}@media (min-width:640px){.uk-container{padding-left:30px;padding-right:30px}}@media (min-width:960px){.uk-container{padding-left:40px;padding-right:40px}}.uk-container::before,.uk-container::after{content:"";display:table}.uk-container::after{clear:both}.uk-container>:last-child{margin-bottom:0}.uk-container-small{max-width:900px}.uk-search{display:inline-block;position:relative;max-width:100%;margin:0}.uk-search-input::-webkit-search-cancel-button,.uk-search-input::-webkit-search-decoration{-webkit-appearance:none}.uk-search-input::-moz-placeholder{opacity:1}.uk-search-input{box-sizing:border-box;margin:0;border-radius:0;font:inherit;overflow:visible;-webkit-appearance:none;vertical-align:middle;width:100%;border:none;color:#323247}.uk-search-input:-ms-input-placeholder{color:#6e747c!important}.uk-search .uk-search-icon-flip{right:0;left:auto}.uk-search-default{width:180px}.uk-search-default .uk-search-input{height:40px;padding-left:6px;padding-right:6px;background:#fff;border:1px solid #f5f5f5}.uk-search-default .uk-search-icon-flip~.uk-search-input{padding-right:40px}.uk-subnav{display:flex;flex-wrap:wrap;margin-left:-20px;padding:0;list-style:none}.uk-subnav>*{flex:none;padding-left:20px;position:relative}.uk-subnav>*>:first-child{display:block;color:#8f919e;font-size:.875rem;text-transform:none}:root{--uk-leader-fill-content: .}[class*=uk-width]{box-sizing:border-box;width:100%;max-width:100%}.uk-text-lead{font-size:1.125rem;line-height:1.5;color:#8f919e}.uk-text-small{font-size:.875rem;line-height:1.5}.uk-text-muted{color:#8f919e!important}.uk-text-center{text-align:center!important}.uk-box-shadow-large{box-shadow:0 10px 35px rgba(0,0,0,.12)}.hero-image{margin-top:-28px;margin-bottom:30px}.hero-image img{max-height:220px}.uk-section .uk-text-lead{margin:0}.section-hero{z-index:10}.section-hero .hero-search{margin:60px auto 0}.section-hero .hero-search .uk-search-input{height:80px;border-radius:50px;font-size:1.25rem;font-style:normal;padding-left:36px}.section-hero .uk-text-lead{font-size:1.25rem;color:#686c7b}.uk-flex-center{justify-content:center}.uk-margin-medium{margin-bottom:40px}*+.uk-margin-medium{margin-top:40px!important}.uk-position-relative{position:relative!important}
+</style>
+<title>Indian 4 You - Smart Search Engine on the Web!</title>
+<meta name="description" content="Indian 4 You, Our mission is to organise the world’s information by most search categories and make it universally simple accessible and very useful." />
+<meta name="keywords" content="Search Engine, Indian 4 You" />
+<script data-ad-client="ca-pub-3536833820610759" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body>
+<header>
+<div class="uk-section section-hero uk-position-relative" data-uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true">
+<div class="uk-container uk-container-small">
+<p class="hero-image uk-text-center"><a href="/"><img src="static/image/logo.png" alt="I4U"></a></p>
+<h1 class="uk-heading-medium uk-text-center" style="color: #029fe2;font-weight: 600;font-size: xx-large;margin-top: -50px; margin-bottom: -25px;">Smart Search Engine For Indians.</h1>
+<div class="hero-search uk-text-center">
+<div class="uk-position-relative">
+<form method="get" class="uk-search uk-search-default uk-width-1-1" name="search-hero" action="/search/result.php">
+<span class="uk-search-icon-flip" data-uk-search-icon></span>
+<input id="search-hero" class="uk-search-input uk-box-shadow-large" type="search" placeholder="Search..." name="s" title="search" style="height: 75px;border: 2px solid #029fe2;box-shadow: none;border    -radius: 10px;padding-left: 25px;">
+</form>
+</div>
+</div>
+<p class="uk-text-lead uk-text-center" style="margin-top: 25px;">Search Now & Find The Best Results For Whatever You Are Looking For...</p>
+</div>
+</div>
+</header>
+<footer class="uk-section uk-text-center uk-text-muted">
+<div class="uk-container uk-container-small">
+<div>
+<ul class="uk-subnav uk-flex-center">
+<li><a href="static/about.php">About</a></li>
+<li><a href="static/privacy-policy.php">Privacy</a></li>
+<li><a href="static/cookies-policy.php">Cookies</a></li>
+<li><a href="static/terms-of-service.php">Terms</a></li>
+<li><a href="mailto:info@indian4you.com?Subject=Contact%20Us" target="_top">Contact</a></li>
+</ul>
+</div>
+<div class="uk-margin-medium uk-text-small copyright link-secondary">
+<p>All logos, trademarks and registered trademarks are the property of their respective owners.</p>
+<p>All rights reserved. © 2015-2020 Indian4you.com.</p>
+</div>
+</div>
+</footer>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NL8M5ZH');</script>
+<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NL8M5ZH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<link rel="stylesheet" href="static/css/main.css"/>
+<script async src="static/js/uikit.js"></script>
+<script async src="static/js/custom.js"></script>
+</body>
+</html>
